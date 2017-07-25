76b9a555df12e4c1941f7402909dcbcf7f491339
Linesh <linesh.simpcity@gmail.com>
2017-07-25 14:43:44 +0800

    [Congyu] 🚨Test: add Enzyme component test for Header

---
+++ src/components/Header.spec.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { Header as ReactNativeHeader } from 'react-native-elements'
+import Header from './Header'
+
+describe('Header component', () => {
+  it('should render a Header component', () => {
+    const component = shallow(<Header />);
+
+    expect(component.find(ReactNativeHeader)).toHaveLength(1)
+  });
+})
