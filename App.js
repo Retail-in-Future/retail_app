ce9bab951cb260a3202c9a6b668e2380070d4a2f
Linesh <linesh.simpcity@gmail.com>
2017-07-25 16:30:49 +0800

    [Congyu] ♻️Refactor: rename tabBar.js to TabBar.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
 
-import TabBar from './src/components/tabBar'
+import TabBar from './src/components/TabBar'
 
 const styles = StyleSheet.create({
   container: {
