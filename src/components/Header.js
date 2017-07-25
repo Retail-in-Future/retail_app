c79923e4f23c7bff209233a3afcea5a8708655d2
Linesh <linesh.simpcity@gmail.com>
2017-07-25 12:33:05 +0800

    [Congyu] 📝Todo: add two todos of adding Enzyme and Eslint

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { Header } from 'react-native-elements';
 
+// TODO: [Linesh][7/25/17] add Enzyme component unit test support
+// TODO: [Linesh][7/25/17] add eslint
 export default ({ title = 'I am not the title' }) => (
   <Header outerContainerStyles={styles.headerOuter}
           centerComponent={{ text: title }}
