84ec2034badce4f20b2f754e92b4e8a84648bc47
Linesh <linesh.simpcity@gmail.com>
2017-07-25 17:01:30 +0800

    [Congyu] ✨Feature: add username, password input field and a login button

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,7 @@
-import React from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+/* eslint-disable class-methods-use-this */
+import React, { Component } from 'react'
+import { View, StyleSheet, TextInput } from 'react-native'
+import { Button } from 'react-native-elements'
 
 import Header from '../components/Header'
 
@@ -9,9 +11,49 @@ const styles = StyleSheet.create({
   }
 })
 
-export default () => (
-  <View style={styles.container}>
-    <Header />
-    <Text>I am home page.</Text>
-  </View>
-)
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      username: '',
+      password: ''
+    }
+
+    this.onUsernameChanged = this.onUsernameChanged.bind(this)
+    this.onPasswordChanged = this.onPasswordChanged.bind(this)
+    this.onLogin = this.onLogin.bind(this)
+  }
+
+  onUsernameChanged(username) {
+    this.setState({ username })
+  }
+
+  onPasswordChanged(password) {
+    this.setState({ password })
+  }
+
+  onLogin() {}
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header />
+        <TextInput style={{ width: 200, height: 44, padding: 8 }}
+          value={this.state.username}
+          onChangeText={this.onUsernameChanged}
+        />
+        <TextInput style={{ width: 200, height: 44, padding: 8 }}
+          value={this.state.password}
+          onChangeText={this.onPasswordChanged}
+        />
+        <Button raised
+          icon={{ name: 'home', size: 32 }}
+          buttonStyle={{ backgroundColor: '#ff4f00', borderRadius: 5 }}
+          textStyle={{ textAlign: 'center' }}
+          title="Login"
+          onPress={this.onLogin}
+        />
+      </View>
+    )
+  }
+}
