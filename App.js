5d9cf22625f42a50f6bae8a331d969a864213e61
Linesh <linesh.simpcity@gmail.com>
2017-07-31 14:56:06 +0800

    [#30][Linesh] ♻️Refactor: export default component in index.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
 
-import TabBar from './src/components/TabBar/TabBar'
+import TabBar from './src/components/TabBar'
 
 const styles = StyleSheet.create({
   container: {
