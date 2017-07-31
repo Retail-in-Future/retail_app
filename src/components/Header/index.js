194560f8b6a8dfa0b82af6aabcfe5060a607756a
Linesh <linesh.simpcity@gmail.com>
2017-07-31 15:02:33 +0800

    [#30][Linesh] ♻️Refactor: rename internal CommonHeader to Header, use defaultProps

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Header } from 'react-native-elements'
+
+import { Header as ReactNativeHeader } from 'react-native-elements'
 
 import styles from './styles'
 
-const CommonHeader = ({ title = 'I am not the title' }) => (
-  <Header outerContainerStyles={styles.headerOuter}
+const Header = ({ title }) => (
+  <ReactNativeHeader outerContainerStyles={styles.headerOuter}
     centerComponent={{ text: title }}
   />
 )
 
-CommonHeader.propTypes = {
+Header.propTypes = {
   title: PropTypes.string
 }
 
-export default CommonHeader
+Header.defaultProps = {
+  title: 'Give me title'
+}
+
+export default Header
 
