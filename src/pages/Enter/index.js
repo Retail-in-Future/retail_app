d466caebd172fcf0aa593fdeb15ce96912b98416
Linesh <linesh.simpcity@gmail.com>
2017-08-01 17:15:28 +0800

    [#41][Linesh] ♻️Refactor: move redux action directly to this.props through mapDispatchToProps

--- src/pages/Enter/index.js
+++ src/pages/Enter/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 
 import { View } from 'react-native'
 import { Text, Button } from 'react-native-elements'
--- src/pages/Login/LoginRequired.js
+++ src/pages/Login/LoginRequired.js
@@ -1,36 +1,25 @@
 /* eslint-disable import/no-named-as-default */
 import React, { Component } from 'react'
-import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
 import _ from 'lodash'
 
 import Login from './Login/Login'
-import * as actionCreators from './actions'
+import { login } from './actions'
 
 class LoginRequired extends Component {
-  constructor() {
-    super()
-
-    this.onLogin = this.onLogin.bind(this)
-  }
-
-  onLogin(username, password) {
-    this.props.login(username, password)
-  }
-
   isLoggedIn() {
     return !_.isEmpty(this.props.token)
   }
 
   render() {
-    const { children, errorMessage } = this.props
+    const { children, onLogin, errorMessage } = this.props
 
     return (
       this.isLoggedIn() ?
         children :
-        <Login onLogin={this.onLogin} errorMessage={errorMessage} />
+        <Login onLogin={onLogin} errorMessage={errorMessage} />
     )
   }
 }
@@ -39,13 +28,19 @@ LoginRequired.propTypes = {
   children: PropTypes.node,
   token: PropTypes.string,
   errorMessage: PropTypes.string,
-  login: PropTypes.func
+  onLogin: PropTypes.func
 }
 
+const mapStateToProps = state => ({
+  token: state.login.token,
+  errorMessage: state.login.errorMessage
+})
+
+const mapDispatchToProps = dispatch => ({
+  onLogin: (username, password) => dispatch(login(username, password))
+})
+
 export default connect(
-  state => ({
-    token: state.login.token,
-    errorMessage: state.login.errorMessage
-  }),
-  dispatch => bindActionCreators(actionCreators, dispatch)
+  mapStateToProps,
+  mapDispatchToProps
 )(LoginRequired)
