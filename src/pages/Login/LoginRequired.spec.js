d8bd6734324587344edf49c3753f76f7598212e5
Linesh <linesh.simpcity@gmail.com>
2017-08-02 14:32:59 +0800

    [#41][Linesh] 📝Todo: remove todo, now we don't need setState() feature

--- src/pages/Login/LoginRequired.spec.js
+++ src/pages/Login/LoginRequired.spec.js
@@ -7,9 +7,6 @@ import Login from './Login/Login'
 import { LoginRequired } from './LoginRequired'
 
 describe('Login component', () => {
-  // TODO: [Linesh][7/30/17] Enable these 2 tests after React 16 is officially released
-  // and supported by Enzyme. Now Enzyme does not support React 16 alpha.
-  // For more context: https://github.com/airbnb/enzyme/issues/1051
   it('should render login page when an authentication token is not present', () => {
     const component = shallow(<LoginRequired token="" />)
 
