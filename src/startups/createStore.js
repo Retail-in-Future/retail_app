6d498a5c194c6070a8ec99a41cc77a6d7c7be06f
Linesh <linesh.simpcity@gmail.com>
2017-08-02 18:29:11 +0800

    [#41][Linesh] 🔧Chore: remove redux-logger

--- src/startups/createStore.js
+++ src/startups/createStore.js
@@ -1,21 +1,14 @@
-/* global __DEV__ */
-/* eslint-disable import/no-extraneous-dependencies */
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
-import { createLogger } from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 import reducers from '../reducers'
-
-const loggerMiddleware = createLogger({ predicate: () => __DEV__ })
 
 export default () => {
   const initState = {}
   const middlewares = composeWithDevTools(
     applyMiddleware(
-      // remove any middleware when no longer necessary
-      thunkMiddleware,
-      loggerMiddleware
+      thunkMiddleware
     )
   )
 
