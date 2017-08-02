fd2556d3426ee2d6ef44f24d8d47e2edc9c55118
Linesh <linesh.simpcity@gmail.com>
2017-08-02 18:11:19 +0800

    [#41][Linesh] ✨Feature: inline App.styles.js

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 
 import createStore from './src/startups/createStore'
 import TabBar from './src/components/TabBar'
 
-import styles from './App.styles'
+const store = createStore()
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
 
 export default () => (
-  <Provider store={createStore()}>
+  <Provider store={store}>
     <View style={styles.container}>
       <TabBar />
     </View>
