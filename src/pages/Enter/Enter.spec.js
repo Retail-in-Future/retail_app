c6c1d603b8901f3be497c548ea4af7a888376160
Linesh <linesh.simpcity@gmail.com>
2017-08-02 19:08:04 +0800

    [#41][Linesh] 🎨Style: remove dependencies, this is a wrong test to add..

--- src/pages/Enter/Enter.spec.js
+++ src/pages/Enter/Enter.spec.js
@@ -1,7 +1,3 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-
-import { Enter } from '.'
 import Header from '../../components/Header'
 
 describe.skip('Enter component', () => {
@@ -9,7 +5,7 @@ describe.skip('Enter component', () => {
 
   beforeEach(() => {
     // https://github.com/facebook/react-native/issues/12440
-    component = shallow(<Enter username="admin" qrcode="admin$token" />)
+    // component = shallow(<Enter username="admin" qrcode="admin$token" />)
   })
 
   it('should render a header with title 扫码进入', () => {
