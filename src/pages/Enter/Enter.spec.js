b576c3fc1a212ba14a6404b36058a3874789d197
Linesh <linesh.simpcity@gmail.com>
2017-08-02 19:05:41 +0800

    [#41][Linesh] 🚨Test: skip the whole test container

--- src/pages/Enter/Enter.spec.js
+++ src/pages/Enter/Enter.spec.js
@@ -4,15 +4,15 @@ import { shallow } from 'enzyme'
 import { Enter } from '.'
 import Header from '../../components/Header'
 
-describe('Enter component', () => {
+describe.skip('Enter component', () => {
   let component
 
   beforeEach(() => {
+    // https://github.com/facebook/react-native/issues/12440
     component = shallow(<Enter username="admin" qrcode="admin$token" />)
   })
 
-  it.skip('should render a header with title 扫码进入', () => {
-    // https://github.com/facebook/react-native/issues/12440
+  it('should render a header with title 扫码进入', () => {
     expect(component.find(Header)).toHaveLength(1)
   })
 })
