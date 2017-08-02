7c1dee1d0a5e4e5667e7df8a58e3ccddf58061c7
Linesh <linesh.simpcity@gmail.com>
2017-08-02 12:03:40 +0800

    [#41][Linesh] 🚨Test: add test, should call url and dispatch actions on success

--- src/actions/login.actions.spec.js
+++ src/actions/login.actions.spec.js
@@ -1,5 +1,9 @@
-import { clearErrorMessage, updateErrorMessage, updateUsername } from './login'
-import { UPDATE_ERROR_MESSAGE, UPDATE_USERNAME } from './actionTypes'
+import qs from 'querystring'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+
+import { clearErrorMessage, login, updateErrorMessage, updateUsername } from './login'
+import { RECEIVE_TOKEN, UPDATE_ERROR_MESSAGE, UPDATE_USERNAME } from './actionTypes'
 
 describe('Login action creators', () => {
   it('should return an UPDATE_USERNAME action with username when updateUsername is called', () => {
@@ -40,4 +44,53 @@ describe('Login action creators', () => {
     expect(result).toEqual(expected)
   })
 
+  describe('async action creators', () => {
+    let mock
+    const authenticationUrl = 'http://10.207.11.201:9000/auth/realms/master/protocol/openid-connect/token'
+    const params = (username, password) => qs.stringify({
+      username,
+      password,
+      grant_type: 'password',
+      client_id: 'retail'
+    })
+
+    it('should call authentication url and dispatch RECEIVE_TOKEN action and CLEAR_ERROR_MESSAGE action when successfully authenticated', async () => {
+      const username = 'admin'
+      const password = 'admin'
+      const dispatch = jest.fn()
+      const expectedClearErrorMessageAction = {
+        type: UPDATE_ERROR_MESSAGE,
+        payload: {
+          errorMessage: ''
+        }
+      }
+      const expectedReceiveTokenAction = {
+        type: RECEIVE_TOKEN,
+        payload: {
+          token: '1234'
+        }
+      }
+
+      mock.onPost(authenticationUrl, params(username, password)).reply(200, {
+        access_token: '1234'
+      })
+
+      await login(username, password)(dispatch)
+
+      expect(dispatch).toHaveBeenCalledWith(expectedClearErrorMessageAction)
+      expect(dispatch).toHaveBeenCalledWith(expectedReceiveTokenAction)
+    })
+
+    beforeAll(() => {
+      mock = new MockAdapter(axios)
+    })
+
+    afterEach(() => {
+      mock.reset()
+    })
+
+    afterAll(() => {
+      mock.restore()
+    })
+  })
 })
