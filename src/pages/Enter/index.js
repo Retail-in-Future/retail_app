b027ff7193faeba9d58636c76638eb11e4c2dffd
Linesh <linesh.simpcity@gmail.com>
2017-08-04 15:16:26 +0800

    [#8][Linesh] ♻️Refactor: separate QRCode as separate component

--- src/pages/Enter/index.js
+++ src/pages/Enter/index.js
@@ -3,9 +3,8 @@ import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
 import { View } from 'react-native'
-import { Text, Button } from 'react-native-elements'
 
-import QrCode from 'react-native-qrcode'
+import QRCode from '../../components/QRCode'
 
 import Header from '../../components/Header'
 import styles from './styles'
@@ -13,49 +12,29 @@ import * as actionCreators from '../../actions/enter'
 
 export class Enter extends Component {
   componentDidMount() {
-    this.props.generateQRCode(this.props.username)
+    this.props.generateQRCode()
   }
 
   render() {
-    const { qrcode, generateQRCode, username } = this.props
-
     return (
       <View style={styles.container}>
         <Header title="扫码进入" />
-        <View style={styles.body}>
-          {
-            qrcode ?
-              <View>
-                <QrCode value={qrcode} size={300} />
-                <Text style={styles.text}>请将手机屏幕对准扫描器</Text>
-              </View>
-              : <Text style={styles.text}>二维码生成失败。请刷新重试……</Text>
-          }
-
-          <Button icon={{ name: 'refresh' }}
-            backgroundColor="#03A9F4"
-            onPress={() => generateQRCode(username)}
-            buttonStyle={styles.buttonStyle}
-            title="刷新二维码"
-          />
-        </View>
+        <QRCode {...this.props} />
       </View>
     )
   }
 }
 
 Enter.propTypes = {
-  username: PropTypes.string,
   qrcode: PropTypes.string,
   generateQRCode: PropTypes.func
 }
 
 export default connect(
   state => ({
-    username: state.credentials.username,
     qrcode: state.enter.qrcode
   }),
   dispatch => ({
-    generateQRCode: username => dispatch(actionCreators.generateQRCode(username))
+    generateQRCode: () => dispatch(actionCreators.generateQRCode())
   })
 )(Enter)
--- src/pages/Enter/styles.js
+++ src/pages/Enter/styles.js
@@ -3,22 +3,5 @@ import { StyleSheet } from 'react-native'
 export default StyleSheet.create({
   container: {
     flex: 1
-  },
-  body: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  text: {
-    textAlign: 'center',
-    marginTop: 20,
-    marginBottom: 20
-  },
-  buttonStyle: {
-    borderRadius: 0,
-    marginLeft: 0,
-    marginRight: 0,
-    marginBottom: 0
   }
 })
